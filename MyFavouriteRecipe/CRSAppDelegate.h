396c9ab1bdbc3be5a10542e871480486ab15f206
Carlos Roig Salvador <roxeman3@gmail.com>
2014-05-13 20:28:36 +0200

    Initial Commit

---
+++ MyFavouriteRecipe/CRSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRSAppDelegate.h
+//  MyFavouriteRecipe
+//
+//  Created by Carlos Roig Salvador on 13/05/14.
+//  Copyright (c) 2014 RoxeLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFavouriteRecipeTests/MyFavouriteRecipeTests.m
@@ -0,0 +1,34 @@
+//
+//  MyFavouriteRecipeTests.m
+//  MyFavouriteRecipeTests
+//
+//  Created by Carlos Roig Salvador on 13/05/14.
+//  Copyright (c) 2014 RoxeLabs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFavouriteRecipeTests : XCTestCase
+
+@end
+
+@implementation MyFavouriteRecipeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
